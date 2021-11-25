d24824c1ca35efc69e8bc9a639d6ff2be1a8d4dd
Sean Robertson <sean@robertson-family.nz>
2021-11-25 16:04:25 +1300

    Update bookmarklet.js
    Switch to input source `prompt()` rather than HB localStorage.
    Remove debugging lines.

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,6 @@
 javascript:(function(){
-const inputText = localStorage.getItem('homebrewery-new');
-  if(location.href=='https://homebrewery.naturalcrit.com/new' && confirm('Process this text?\n' + inputText.substring(0,250))){
+if(location.href=='https://homebrewery.naturalcrit.com/new'){
+	const inputText = prompt('Enter source text:');
 	  const replacementList = [
 		  {term: "\\\\pagebreaknum", text: "{{pageNumber,auto}}\n\\page"},
 		  {term: "@=====", text:"{{pageNumber,auto}}\n\\page"},
@@ -11,8 +11,6 @@ const inputText = localStorage.getItem('homebrewery-new');
 	  ];
 	  var outputText = inputText;
 	  for (const replacement of replacementList){
-		  console.log(replacement.term);
-		  console.log(replacement.text);
 		  outputText = outputText.replace(new RegExp(replacement.term, 'ig'), replacement.text);
 	  }
 	  localStorage.setItem('homebrewery-new', outputText);
