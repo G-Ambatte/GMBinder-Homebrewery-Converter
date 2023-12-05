b4a9eb98cecacf4ec55835e0d5e144e220f29204
G.Ambatte <sean@robertson-family.nz>
2023-12-05 18:54:08 +1300

    Fix for monster blocks

--- src/bookmarklet.js
+++ src/bookmarklet.js
@@ -7,33 +7,22 @@ if(location.href=='https://homebrewery.naturalcrit.com/new'){
 		  {term: "\\\\pagebreak", text: "\\page"},
 		  {term: "======", text: "\\page"},
 		  {term: "\\\\columnbreak", text: "\\column"},
-		  {term: "(?:^[^>]?)(\n)(?=^>)", text: "{{monster,frame\n"},
-		  {term: "(?:^>.*?$)(\\n)(?!^>)", text: "$&}}\n"},
-		  {term: "^(>-(\\s*))", text: ""},
-		  {term: "^(>(\\s*))", text: ""},
-		  {term: "^(\\*\\*Armor Class\\*\\*)", text: "**Armor Class** ::"},
-		  {term: "^(\\*\\*Hit Points\\*\\*)", text: "**Hit Points** ::"},
-		  {term: "^(\\*\\*Speed\\*\\*)", text: "**Speed** ::"},
-		  {term: "^(\\*\\*Skills\\*\\*)", text: "**Skills** ::"},
-		  {term: "^(\\*\\*Saving Throws\\*\\*)", text: "**Saving Throws** ::"},
-		  {term: "^(\\*\\*Damage Vulnerabilities\\*\\*)", text: "**Damage Vulnerabilities** ::"},
-		  {term: "^(\\*\\*Damage Resistances\\*\\*)", text: "**Damage Resistances** ::"},
-		  {term: "^(\\*\\*Damage Immunities\\*\\*)", text: "**Damage Immunities** ::"},
-		  {term: "^(\\*\\*Condition Immunities\\*\\*)", text: "**Condition Immunities** ::"},
-		  {term: "^(\\*\\*Senses\\*\\*)", text: "**Senses** ::"},
-		  {term: "^(\\*\\*Languages\\*\\*)", text: "**Languages** ::"},
-		  {term: "^(\\*\\*Challenge\\*\\*)", text: "**Challenge** ::"},
-		  {term: "^(\\*\\*Proficiency Bonus\\*\\*)", text: "**Proficiency Bonus** ::"},
+		  {term: "(\\n>[\\s\\S]*?)(?:\\n[^>])", text: "\n{{monster,frame\n$1\n}}\n", flags: "gi"},
+		  {term: "^>\\s*?-\\s*", text: ""},
+		  {term: "^(>\\s*)", text: ""},
+		  {term: "^(\\*\\*.+\\*\\*)(.*)", text: "$1 :: $2"},
 		  {term: ".phb", text: ".page"}
 	  ];
 	  var outputText = '\n' + inputText + '\n';
-	  console.log(outputText);
 	  for (const replacement of replacementList){
-		  outputText = outputText.replace(new RegExp(replacement.term, 'gim'), replacement.text);
+		  outputText = outputText.replace(new RegExp(replacement.term, replacement.flags || 'gim'), replacement.text);
+		  console.log(replacement.term);
+		  console.log(replacement.flags || 'gim');
+		  console.log(outputText);
 	  }
 	  localStorage.setItem('homebrewery-new', outputText);
 	  localStorage.removeItem('homebrewery-new-style');
 	  localStorage.setItem('homebrewery-new-meta','{"renderer":"V3"}');
-	  location.reload(); 
+	  location.reload(); 	  
 	  };
 })();
